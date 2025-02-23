cb2db2fb909bf5af61b89bd671d4f91e2330321a
Mr-Confidence <145501133+Mr-Confidence@users.noreply.github.com>
2025-02-23 20:43:02 +0000

    My Company

---
+++ my-company/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Services from "./Services";
+import Contact from "./Contact";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
